705e471e97cc3b4a9db8a29d4e0928780a39f522
maroussiark <soamaroussia@gmail.com>
2023-03-23 22:54:38 +0300

    Sprint5

---
+++ Framework/src/java/etu1833/framework/view/ModelView.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1833.framework.view;
+
+/**
+ *
+ * @author maroussia
+ */
+public class ModelView {
+    private String view;
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    public ModelView() {
+    }
+    
+}
